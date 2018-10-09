d557a776cac868de20e18b4471d98b6a6e08496c
Seb Bacon <seb.bacon@gmail.com>
2018-10-09 10:33:06 +0100

    Add a simple test

---
+++ test_general.py
@@ -0,0 +1,16 @@
+from htmlslacker import HTMLSlacker
+
+
+def test_example_1():
+    html = """
+    <b>Hello</b><br>
+    There is <i>something</i> interesting about <code>this doc</code>
+
+    <p>
+    And <a href="http://example.com/">here is a
+    link in a paragraph!</a>
+    </p>
+    """
+    expected = "*Hello*\n There is _something_ interesting about `this doc` \n And <http://example.com/|here is a link in a paragraph!>"
+    output = HTMLSlacker(html).get_output()
+    assert(output == expected)
