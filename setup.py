ab567938f0f156828989b0bb93f75aa7d6d0b572
N0str <ntpcp@yandex.ru>
2018-10-11 13:15:28 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pytest_runner = ['pytest-runner'] if needs_pytest else []
 setup(
     name='html-slacker',
     packages=find_packages(),
-    version='0.1.4',
+    version='0.1.5',
     description='Converts HTML to Slack formatted markdown',
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
     keywords='slack bot html convert markdown slackbot pythonbot slack html markdown',
